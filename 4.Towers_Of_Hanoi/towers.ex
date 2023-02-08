b4f1338ad982491d256dbd78d4cce0e10865cdd5
vrncndlr <veronica.j.nadler@gmail.com>
2023-02-08 15:52:55 +0100

    assignment 4

--- 4.Towers_Of_Hanoi/towers.ex
+++ 4.Towers_Of_Hanoi/towers.ex
@@ -1,26 +1,22 @@
 defmodule Towers do
   #base case
   def hanoi(0, _, _, _) do [] end
-  def hanoi(1, :a, _, :c) do [ {:move, :a, :c} ] end
-  def hanoi(2, :a, :b, :c) do
-    [ {:move, :a, :b}
-    {:move, :a, :c},
-    {:move, :b, :c} ]
-  end
-  def hanoi(3, :a, :b, :c) do
-    [ {:move, :a, :c},
-      {:move, :a, :b},
-      {:move, :c, :b},
-      {:move, :a, :c},
-      {:move, :b, :a},
-      {:move, :b, :c},
-      {:move, :a, :c} ]
-  end
   #recursive
-  #Three sequences:
-#1. moving a smaller tower from from to an auxiliary peg (using the third peg as the auxiliary),
-#2. moving the last disc from from to to
-#3. moving the smaller tower from the auxiliary peg to to (using the em from peg as the auxiliary).
-  def hanoi(n, from, aux, to) do ... end
+  #Three steps:
+  #1. moving a smaller tower from from to an auxiliary peg (using the third peg as the auxiliary),
+  #2. moving the last disc from from to to
+  #3. moving the smaller tower from the auxiliary peg to to (using the from peg as the auxiliary).
+  def hanoi(n, from, aux, to) do
+      hanoi((n-1), from, to, aux) ++
+      [{:move, from, to}] ++
+      #hanoi(1, from, aux, to) ++
+      hanoi((n-1), aux, from, to)
+  end
+
+  def moves(list) do Enum.count(list) end
 
 end
+
+#c("towers.ex")
+#list = Towers.hanoi(4, :a,:b,:c)
+#Towers.moves(list)
